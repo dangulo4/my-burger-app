35247a4fb5eb3295534f4f518e06388557e9fa9a
Daniel Angulo <dangulo4@gmail.com>
2021-03-11 18:52:38 -0800

    Added toolbar and navigation with logo

--- src/components/Burger/OrderSummary/index.js
+++ src/components/Burger/OrderSummary/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Aux from '../../../hoc/Auxillary';
+import Button from '../../UI/Button';
 
 const orderSummary = (props) => {
   const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
@@ -16,7 +17,16 @@ const orderSummary = (props) => {
       <h3>Your Order</h3>
       <p>A delicious burger with the following ingredients:</p>
       <ul>{ingredientSummary}</ul>
+      <p>
+        <strong>Total Price: {props.price.toFixed(2)}</strong>
+      </p>
       <p>Continue to Checkout?</p>
+      <Button btnType='Danger' clicked={props.purchaseCancelled}>
+        Cancel
+      </Button>
+      <Button btnType='Success' clicked={props.purchaseContinued}>
+        Continue
+      </Button>
     </Aux>
   );
 };
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
+
 import Aux from '../../hoc/Auxillary';
 import classes from './Layout.module.css';
+import Toolbar from '../Navigation/Toolbar';
 
 const layout = (props) => (
   <Aux>
-    <div>Toolbar, SideDrawer, Backdrop</div>
+    <Toolbar />
     <main className={classes.Content}>{props.children}</main>
   </Aux>
 );
