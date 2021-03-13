a68681bda5435fb833028dbff9940c2312fbaeaa
Daniel Angulo <dangulo4@gmail.com>
2021-03-13 15:21:12 -0800

    Added Sidebar and Navigation functionality

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../hoc/Auxillary';
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer';
 
-const layout = (props) => (
-  <Aux>
-    <Toolbar />
-    <main className={classes.Content}>{props.children}</main>
-  </Aux>
-);
+class Layout extends Component {
+  state = {
+    showSideDrawer: false,
+  };
+  SideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
 
-export default layout;
+  sideDrawerToggleHandler = () => {
+    this.setState((prevState) => {
+      return { showSideDrawer: !prevState.showSideDrawer };
+    });
+  };
+
+  render() {
+    return (
+      <Aux>
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.SideDrawerClosedHandler}
+        />
+        <main className={classes.Content}>{this.props.children}</main>
+      </Aux>
+    );
+  }
+}
+
+export default Layout;
--- src/components/Navigation/Toolbar/index.js
+++ src/components/Navigation/Toolbar/index.js
@@ -3,12 +3,16 @@ import React from 'react';
 import classes from './Toolbar.module.css';
 import Logo from '../../Logo';
 import NavigationItems from '../NavigationItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle';
 
 const toolbar = (props) => (
   <header className={classes.Toolbar}>
-    <div>Menu</div>
-    <Logo />
-    <nav>
+    <DrawerToggle clicked={props.drawerToggleClicked} />
+    <div className={classes.Logo}>
+      <Logo />
+    </div>
+
+    <nav className={classes.DesktopOnly}>
       <NavigationItems />
     </nav>
   </header>
